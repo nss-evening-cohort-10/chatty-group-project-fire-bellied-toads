f43625fab2177b01702a073e7277bbc6dd700b1a
Laura Collins <laura.e.collins1@gmail.com>
2019-10-17 20:09:12 -0500

    delete button is working

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,11 +3,13 @@ import '@fortawesome/fontawesome-free/js/all';
 import '../styles/main.scss';
 import data from './helpers/data/messageData';
 import display from './components/displayMsgs/displayMsgs';
+import deleteMsg from './components/deleteMsgs/deleteMsg';
 
 const msgArray = data.getMessage();
 
 const init = () => {
   display.displayMessages(msgArray);
+  deleteMsg.deleteMessage();
 };
 
 init();
