f99c481c345b2de5a9d26e694a970026c1445b27
Ashley Claiborne <languageisaliquid@gmail.com>
2019-10-19 17:17:17 -0500

    added nav media queries

--- src/javascripts/components/editMsg/editMsg.js
+++ src/javascripts/components/editMsg/editMsg.js
@@ -7,7 +7,7 @@ const restoreForm = () => {
   $('#username').show().prop('required', true);
   $('#message').val('');
   $('#updateBtn').remove();
-  $('#toggleSend').append('<button type="submit" id="sendBtn" class="btn btn-dark col-2">SEND</button>');
+  $('#toggleSend').append('<button type="submit" id="sendBtn" class="btn btn-dark col-2 offset-1">SEND</button>');
 };
 
 const updateMsg = (seq) => {
@@ -29,7 +29,7 @@ const editMsg = (e) => {
   $('#message').val(toEdit);
   $('#sendBtn').remove();
   $('#username').hide().prop('required', false);
-  $('#toggleSend').append('<button type="submit" id="updateBtn" class="btn btn-dark col-2">Update</button>');
+  $('#toggleSend').append('<button type="submit" id="updateBtn" class="btn btn-dark col-2 offset-1">Update</button>');
   $('#updateBtn').click(() => updateMsg(editSeq));
 };
 
--- src/javascripts/components/nav/nav.js
+++ src/javascripts/components/nav/nav.js
@@ -4,26 +4,27 @@ import './nav.scss';
 const printNav = () => {
   let domString = '';
   domString = `
-      <nav class="navbar fixed-top">
-        <div class="col-2">
+      <nav class="navbar fixed-top row d-flex flex-wrap">
+        <div id='logoDiv' class="col-2">
           <img id="navImg" src="/src/assets/images/toadtalk.png" alt="ToadTalk Logo">
         </div>
-        <div class="col-10">
+        <div id='formDiv' class="col-10">
         <form onsubmit='return false'>
           <div id='toggleSend' class="form-group row">
-            <input type="text" class="form-control col-10" id="message" placeholder="What's on your mind?" required>
-            <button type="submit" id="sendBtn" class="btn btn-dark col-2">SEND</button>
+            <input type="text" class="form-control col-9" id="message" placeholder="What's on your mind?" required>
+            <button type="submit" id="sendBtn" class="btn btn-dark col-2 offset-sm-1">SEND</button>
           </div>
-          <div class="form-group row">
+          <div class="form-group row no-gutters">
             <input type="text" class="form-control col-6" id="username" placeholder="Username" required>
-            <div class="col-1"></div>
-            <div class="custom-control custom-switch">
-              <input type="checkbox" class="custom-control-input" id="darkMode">
-              <label class="custom-control-label" for="darkMode"><i class="fas fa-cloud-moon icon" alt="dark mode"></i></label>
-            </div>
-            <div class="custom-control custom-switch">
-              <input type="checkbox" class="custom-control-input" id="textSize">
-              <label class="custom-control-label" for="textSize"><i class="fas fa-text-height icon" alt="enlarge text"></i></label>
+            <div id='switchDiv' class='row d-flex flex-wrap col-3 offset-1'>
+              <div class="custom-control custom-switch">
+                <input type="checkbox" class="custom-control-input" id="darkMode">
+                <label class="custom-control-label" for="darkMode"><i class="fas fa-cloud-moon icon" alt="dark mode"></i></label>
+              </div>
+              <div class="custom-control custom-switch">
+                <input type="checkbox" class="custom-control-input" id="textSize">
+                <label class="custom-control-label" for="textSize"><i class="fas fa-text-height icon" alt="enlarge text"></i></label>
+              </div>
             </div>
             <button type="button" id="clearBtn" class="btn btn-outline-dark col-2">CLEAR</button>
           </div>
