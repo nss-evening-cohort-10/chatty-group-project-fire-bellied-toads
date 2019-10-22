93be764497df3643b3d7413dce93df21e65f8601
John Thielman <john.thielman@gmail.com>
2019-10-21 19:39:50 -0500

    seed messages in correct order

--- src/javascripts/helpers/data/messageData.js
+++ src/javascripts/helpers/data/messageData.js
@@ -1,18 +1,8 @@
 const messageArr = [
   {
     name: 'Khaleesi',
-    message: 'I have a question about fire-bellied toads: Do they spit fire or is it just in their name; asking for a friend.',
-    timeStamp: 'Sat, Oct 19, 298 9:45 AM',
-  },
-  {
-    name: 'John Snow',
-    message: 'Auntie I don\'t think they do.',
-    timeStamp: 'Sat, Oct 19, 298 9:46 AM',
-  },
-  {
-    name: 'Khaleesi',
-    message: 'How would you know!',
-    timeStamp: 'Sat, Oct 19, 298 9:49 AM',
+    message: 'I need more dragons!! War is coming!!',
+    timeStamp: 'Sat, Oct 19, 298 9:55 AM',
   },
   {
     name: 'Cersei',
@@ -21,8 +11,18 @@ const messageArr = [
   },
   {
     name: 'Khaleesi',
-    message: 'I need more dragons!! War is coming!!',
-    timeStamp: 'Sat, Oct 19, 298 9:55 AM',
+    message: 'How would you know!',
+    timeStamp: 'Sat, Oct 19, 298 9:49 AM',
+  },
+  {
+    name: 'John Snow',
+    message: 'Auntie I don\'t think they do.',
+    timeStamp: 'Sat, Oct 19, 298 9:46 AM',
+  },
+  {
+    name: 'Khaleesi',
+    message: 'I have a question about fire-bellied toads: Do they spit fire or is it just in their name; asking for a friend.',
+    timeStamp: 'Sat, Oct 19, 298 9:45 AM',
   },
 ];
 
