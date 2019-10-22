f6c9f86a42547840982f37267969da79139f3664
Evan Grabenstein <evangdesigns@gmail.com>
2019-10-21 20:16:12 -0500

    WIP

--- src/javascripts/components/emoji/emojiAppender.js
+++ src/javascripts/components/emoji/emojiAppender.js
@@ -3,12 +3,12 @@ import e from '../../helpers/data/emoji';
 const emojis = e.getEmojis();
 
 const findEmoji = (msg) => {
-  const regex = /[:...:]/g;
+  const regex = /:*\w*:/g;
   const myEmoji = msg.match(regex);
   if (myEmoji) {
-    const selectedEmoji = emojis.find((x) => x.code === myEmoji);
-    console.log(selectedEmoji);
-    msg.replace(myEmoji, selectedEmoji.emoji);
+    const selectedEmoji = emojis.find((x) => x.code === myEmoji[0]);
+    const newString = msg.replace(selectedEmoji.code, selectedEmoji.emoji);
+    return newString;
   }
   return msg;
 };
