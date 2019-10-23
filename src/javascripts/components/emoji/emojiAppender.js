d1b4ea55f880363e633ca238b479f41c8245070b
Evan Grabenstein <evangdesigns@gmail.com>
2019-10-22 21:38:23 -0500

    finally got multiple emojis

--- src/javascripts/components/emoji/emojiAppender.js
+++ src/javascripts/components/emoji/emojiAppender.js
@@ -3,11 +3,14 @@ import e from '../../helpers/data/emoji';
 const emojis = e.getEmojis();
 
 const findEmoji = (msg) => {
-  const regex = /:*\w*:/g;
+  const regex = /:\S+:/g;
   const myEmoji = msg.match(regex);
   if (myEmoji) {
-    const selectedEmoji = emojis.find((x) => x.code === myEmoji[0]);
-    const newString = msg.replace(selectedEmoji.code, selectedEmoji.emoji);
+    let newString = msg;
+    myEmoji.forEach((emoji) => {
+      const selectedEmoji = emojis.find((x) => x.code === emoji);
+      newString = newString.replace(selectedEmoji.code, selectedEmoji.emoji);
+    });
     return newString;
   }
   return msg;
